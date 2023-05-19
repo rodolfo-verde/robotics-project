d36919610dbeeb77ab6ff3201dbbb84f9953fc6a
rodolfo <rodolfo.f.verde@gmail.com>
2023-05-19 12:39:05 +0200

    Finished queue system and added gui for testing

--- robot_control/controller.py
+++ robot_control/controller.py
@@ -3,13 +3,40 @@ import numpy as np
 import threading
 import time
 import queue
+from dataclasses import dataclass
 
 _MAX_STEP_SIZE: float = np.deg2rad(0.1)  # radians
 _MOVING_TIME: float = 2 / 1000  # seconds
-_FINAL_POS_TOLERANCE: float = np.deg2rad(0.5)  # radians
+_FINAL_POS_TOLERANCE: float = np.deg2rad(0.4)  # radians
 _DEFAULT_SLEEP_POS: list[float] = [0.0, -1.7999999523162842, 1.5499999523162842, 0.800000011920929, 0.0]
 _SLEEP_POS: list[float] = [-0.00920388475060463, -1.7993595600128174, 1.6444274187088013, 0.777728259563446, 0.0]
 _HOME_POS: list[float] = [0] * 5
+
+
+class GripperMovement:
+    OPEN_AT_START: int = 0
+    CLOSE_AT_START: int = 1
+    OPEN_AT_END: int = 2
+    CLOSE_AT_END: int = 3
+    OPEN: int = 4
+    CLOSE: int = 5
+
+
+# class to hold a "movement unit" that will be added to the queue and processed by the moving daemon
+# name is not final (maybe MovementUnit?)
+@dataclass
+class _MovementUnit:
+    final_joints: np.ndarray = None
+    gripper_movement: int = None
+
+
+@dataclass
+class FinalPose:
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
+    roll: float = 0.0
+    pitch: float = 0.0
 
 
 class Controller:
@@ -22,86 +49,47 @@ class Controller:
         self._moving: bool = False
         self._pause: bool = False
         self._forcing_stop: bool = False
-        self.grasp = self._con.gripper.grasp
-        self.release = self._con.gripper.release
+        self._grasp = self._con.gripper.grasp
+        self._release = self._con.gripper.release
         self._num_joints = len(self.joint_positions)
         self._con.gripper.set_pressure(1.0)
         self._joint_position_queue = queue.Queue()
         self._con.arm.set_trajectory_time(_MOVING_TIME, _MOVING_TIME / 2)
         self._alive = True
 
-        self._moving_thread = threading.Thread(target=self.moving_daemon, daemon=True)
+        self._moving_thread = threading.Thread(target=self._moving_daemon, daemon=True)
         self._moving_thread.start()
 
         # make sure the robot is in the sleep pose in case it was left in the grasp pose from a previous run
         if initialize:
             self._goto_base_pos()
 
-    def add_to_queue(self, joint_positions: list[float]) -> None:
-        self._joint_position_queue.put(np.array(joint_positions))
-
-    def await_queue(self) -> None:
-        while self._moving_thread.is_alive() and not self._joint_position_queue.empty() or self._moving:
-            time.sleep(0.01)
-
-    def clear_queue(self) -> None:
-        # pause the moving daemon
-        self._forcing_stop = True
-        # wait for the queue to empty
-        while not self._joint_position_queue.empty():
-            time.sleep(0.01)
-        self._forcing_stop = False
-
-    def _check_delta(self, joint_positions: np.ndarray) -> bool:
-        return np.all(np.abs(joint_positions - self.joint_positions) < _FINAL_POS_TOLERANCE)
-
-    def moving_daemon(self) -> None:
-        def move_control() -> None:
-            for joint_positions in joint_positions_steps:
-                while self._pause:
-                    if self._forcing_stop:
-                        return
-                    time.sleep(0.01)
-                if self._forcing_stop:
-                    return
-                self._con.arm.set_joint_positions(joint_positions)
-                # verify that the joints moved to the correct position (check if the difference is less than
-                # _FINAL_POS_TOLERANCE) if not print a warning and break
-                if not self._check_delta(joint_positions):
-                    print('Warning: joint positions not reached, breaking...')
-                    break
-
-        while self._alive:
-            while not self._joint_position_queue.empty():
-                desired_joint_positions: np.ndarray = self._joint_position_queue.get()
-                # get the difference between the current and desired joint positions
-                diff = desired_joint_positions - self.joint_positions
-                # check if the absolute difference is less than _FINAL_POS_TOLERANCE and if so do not move (it's
-                # basically already there)
-                if np.all(np.abs(diff) < _FINAL_POS_TOLERANCE):
-                    print('Warning: joint positions already reached, skipping...')
-                    continue
-                self._moving = True
-                # set the steps to give the step size of _MAX_STEP_SIZE or 1 (whichever is greater)
-                steps = max(1, int(np.max(np.abs(diff)) / _MAX_STEP_SIZE))
-                # get the step size for each joint
-                step_size = diff / steps
-                joint_positions_steps = [self.joint_positions + step_size * (i + 1) for i in range(steps)]
-                joint_positions_steps[-1] = desired_joint_positions
-                move_control()
-            self._moving = False
-            time.sleep(0.01)
-
     @property
     def num_joints(self) -> int:
         return self._num_joints
+
+    @property
+    def queue_size(self) -> int:
+        return self._joint_position_queue.qsize()
 
     @property
     def moving(self) -> bool:
         return self._moving
 
     @property
+    def paused(self) -> bool:
+        return self._pause
+
+    @property
+    def gripper_moving(self) -> bool:
+        return self._con.gripper.gripper_moving
+
+    @property
     def joint_positions(self) -> np.ndarray:
+        # return np.array([
+        #     self._con.core.joint_states.position[self._con.core.js_index_map[name]]
+        #     for name in self._con.arm.group_info.joint_names
+        # ])
         return np.array(self._con.arm.get_joint_commands())
 
     @property
@@ -121,26 +109,19 @@ class Controller:
     def resume(self) -> None:
         self._pause = False
 
-    def _goto_base_pos(self):
-        if not self._check_delta(np.array(_SLEEP_POS)):
-            self.go_sleep()
-        self.release()
-
     def go_home(self, wait=True) -> None:
-        # self._uniformly_move(joint_positions=self._HOME_POS, wait=True)
-        self.add_to_queue(_HOME_POS)
+        self.add_to_queue(final_joint=_HOME_POS)
         if wait:
             self.await_queue()
 
     def go_sleep(self, wait=True) -> None:
-        # self._uniformly_move(joint_positions=self._DEFAULT_SLEEP_POS, wait=True)
-        # self._uniformly_move(joint_positions=self._SLEEP_POS, wait=True)
-        self.add_to_queue(_DEFAULT_SLEEP_POS)
-        self.add_to_queue(_SLEEP_POS)
+        self.add_to_queue(final_joint=_DEFAULT_SLEEP_POS)
+        self.add_to_queue(final_joint=_SLEEP_POS)
         if wait:
             self.await_queue()
 
     def shutdown(self) -> None:
+        self.resume()
         self.clear_queue()
         self.resume()
         # set the trajectory time to 2 seconds and the accel time to 0.3 seconds to make the shutdown smoother
@@ -150,9 +131,117 @@ class Controller:
         self._moving_thread.join()
         self._con.shutdown()
 
+    def pick_up(self, down: float = 0.01) -> None:
+        """Pick up the object in under the gripper"""
+        pass
+
+    def add_to_queue(self, *, final_joint: list[float] = None, final_pose: FinalPose = None,
+                     gripper_movement: int = None) -> None:
+        # check if everything is None and if so raise an error
+        if final_joint is None and final_pose is None and gripper_movement is None:
+            raise ValueError('At least one of final_joint, final_pose, or gripper must be specified')
+        # if final_joint is None, use final pose to set final_joint
+        if final_joint is None and final_pose is not None:
+            final_joint, _ = self._con.arm.set_ee_pose_components(x=final_pose.x, y=final_pose.y, z=final_pose.z,
+                                                                  roll=final_pose.roll, pitch=final_pose.pitch)
+        if final_joint is not None:
+            final_joint = np.array(final_joint)
+        self._joint_position_queue.put(_MovementUnit(final_joint, gripper_movement))
+
+    def await_queue(self) -> None:
+        while self._moving_thread.is_alive() and not self._joint_position_queue.empty() or self._moving:
+            time.sleep(0.01)
+
+    def clear_queue(self) -> None:
+        # pause the moving daemon
+        self._forcing_stop = True
+        self.resume()
+        # wait for the queue to empty
+        while not self._joint_position_queue.empty():
+            time.sleep(0.01)
+        self._forcing_stop = False
+
+    def _check_delta(self, joint_positions: np.ndarray) -> bool:
+        return np.all(np.abs(joint_positions - self.joint_positions) < _FINAL_POS_TOLERANCE)
+
+    def _await_gripper(self) -> None:
+        while self.gripper_moving:
+            time.sleep(0.01)
+
     def _get_theta_list_from_pose(self, x: float = None, y: float = None, z: float = None, roll: float = None,
                                   pitch: float = None) -> list[float]:
         return self._con.arm.set_ee_pose_components(x=x, y=y, z=z, roll=roll, pitch=pitch, execute=False)[0]
 
-    def pick_up(self, down: float = 0.01) -> None:
-        self.release()
+    def _moving_daemon(self) -> None:
+        def move_control() -> None:
+            for joint_positions in joint_positions_steps:
+                while self._pause:
+                    if self._forcing_stop:
+                        return
+                    time.sleep(0.01)
+                if self._forcing_stop:
+                    return
+                self._con.arm.set_joint_positions(joint_positions)
+                # verify that the joints moved to the correct position (check if the difference is less than
+                # _FINAL_POS_TOLERANCE) if not print a warning and pause the queue
+                if not self._check_delta(joint_positions):
+                    print('Warning: joint positions not reached, it is recommended to clear the queue. Pausing...')
+                    # maybe break here? or clear the queue? or just continue? or raise an error? or do nothing?
+                    # decide later rn just pause
+                    self._pause = True
+                    # break
+
+        while self._alive:
+            while not self._joint_position_queue.empty():
+                movement_unit = self._joint_position_queue.get()
+                if self._forcing_stop:
+                    continue
+                while self._pause:
+                    if self._forcing_stop:
+                        continue
+                    time.sleep(0.01)
+                desired_joint_positions: np.ndarray = movement_unit.final_joints
+                gripper_movement: int = movement_unit.gripper_movement
+                if gripper_movement is not None and gripper_movement in [GripperMovement.OPEN, GripperMovement.CLOSE,
+                                                                         GripperMovement.OPEN_AT_START,
+                                                                         GripperMovement.CLOSE_AT_START]:
+                    if gripper_movement == GripperMovement.OPEN:
+                        self._release(0)
+                    elif gripper_movement == GripperMovement.CLOSE:
+                        self._grasp(0)
+                    else:
+                        if gripper_movement == GripperMovement.OPEN_AT_START:
+                            self._release(0)
+                        else:
+                            self._grasp(0)
+                        self._await_gripper()
+                if desired_joint_positions is not None:
+                    # get the difference between the current and desired joint positions
+                    diff = desired_joint_positions - self.joint_positions
+                    # check if the absolute difference is less than _FINAL_POS_TOLERANCE and if so do not move (it's
+                    # basically already there)
+                    if np.all(np.abs(diff) < _FINAL_POS_TOLERANCE):
+                        print('Warning: joint positions already reached, skipping...')
+                        continue
+                    self._moving = True
+                    # set the steps to give the step size of _MAX_STEP_SIZE or 1 (whichever is greater)
+                    steps = max(1, int(np.max(np.abs(diff)) / _MAX_STEP_SIZE))
+                    # get the step size for each joint
+                    step_size = diff / steps
+                    joint_positions_steps = [self.joint_positions + step_size * (i + 1) for i in range(steps)]
+                    joint_positions_steps[-1] = desired_joint_positions
+                    move_control()
+                if gripper_movement is not None and gripper_movement in [GripperMovement.OPEN_AT_END,
+                                                                         GripperMovement.CLOSE_AT_END]:
+                    if gripper_movement == GripperMovement.OPEN_AT_END:
+                        self._release(0)
+                    else:
+                        self._grasp(0)
+                self._await_gripper()
+            self._moving = False
+            time.sleep(0.01)
+
+    def _goto_base_pos(self):
+        if not self._check_delta(np.array(_SLEEP_POS)):
+            self.go_sleep()
+        self._release()
--- robot_control/test.py
+++ robot_control/test.py
@@ -1,6 +1,6 @@
 import threading
 import time
-from controller import Controller
+from controller import Controller, FinalPose, GripperMovement
 import tkinter as tk
 
 
@@ -14,54 +14,48 @@ def open_daemon(*, use_sim: bool = False, ros2: bool = True) -> None:
     threading.Thread(target=os.system, args=(command,)).start()
 
 
-def safety_demo(con: Controller) -> None:
-    con.set_join_pose([0, 0, 0, 0, 0])
-    while con.moving:
-        input("Press enter to pause")
-        con.pause()
-        input("Press enter to resume")
-        con.resume()
-
-
 def back_and_forth_demo(con: Controller) -> None:
     x, y, z, roll, pitch = (
         0.19113874615010806, 0.013804476166711615, 0.03837648669453313, -0.00306796166114509, 1.5002332143485546)
     ban = True
-    for _ in range(100):
+    reps = 0
+    while reps < 5:
         x += 0.005 * (-1 if ban else 1)
-        con.set_xyz_pose(x=x, y=y, z=z, roll=roll, pitch=pitch, wait=True)
+        con.add_to_queue(final_pose=FinalPose(x=x, y=y, z=z, roll=roll, pitch=pitch))
         if x < 0.12:
             ban = False
+            con.add_to_queue(gripper_movement=GripperMovement.OPEN)
+            reps += 1
         if x > 0.2:
             ban = True
+            con.add_to_queue(gripper_movement=GripperMovement.CLOSE)
 
 
 def pick_and_place_demo(con: Controller) -> None:
     x, y, z, roll, pitch = (
         0.19113874615010806, 0.013804476166711615, 0.03837648669453313, -0.00306796166114509, 1.5002332143485546)
-    con.set_xyz_pose(x=x, y=y, z=z + .01, roll=roll, pitch=pitch, wait=True)
+    con.add_to_queue(final_pose=FinalPose(x=x, y=y, z=z + .01, roll=roll, pitch=pitch),
+                     gripper_movement=GripperMovement.OPEN)
+    con.await_queue()
     input("Press enter to pick up")
-    con.grasp()
-    con.set_xyz_pose(x=x + .03, y=y, z=z + .05, roll=roll, pitch=pitch, wait=True)
+    con.add_to_queue(final_pose=FinalPose(x=x + .03, y=y, z=z + .05, roll=roll, pitch=pitch),
+                     gripper_movement=GripperMovement.CLOSE_AT_START)
+    con.await_queue()
     time.sleep(1)
-    con.set_xyz_pose(x=x + .07, y=y, z=z + .01, roll=roll, pitch=pitch, wait=True)
-    con.release()
+    con.add_to_queue(final_pose=FinalPose(x=x + .07, y=y, z=z + .01, roll=roll, pitch=pitch),
+                     gripper_movement=GripperMovement.OPEN_AT_END)
+    con.go_sleep(wait=False)
+    con.add_to_queue(final_pose=FinalPose(x=x + .07, y=y, z=z + .01, roll=roll, pitch=pitch),
+                     gripper_movement=GripperMovement.CLOSE_AT_END)
+    con.add_to_queue(final_pose=FinalPose(x=x, y=y, z=z + .01, roll=roll, pitch=pitch),
+                     gripper_movement=GripperMovement.OPEN_AT_END)
+    con.go_home(wait=False)
 
-    con.go_sleep()
 
-    con.set_xyz_pose(x=x + .07, y=y, z=z + .01, roll=roll, pitch=pitch, wait=True)
-    con.grasp()
-    con.set_xyz_pose(x=x, y=y, z=z + .01, roll=roll, pitch=pitch, wait=True)
-    con.release()
-
-
-def pick_and_place_demo2(con: Controller) -> None:
-    x, y, z, roll, pitch = (
-        0.19113874615010806, 0.013804476166711615, 0.03837648669453313, -0.00306796166114509, 1.5002332143485546)
-    con.set_xyz_pose(x=x, y=y, z=z + .01, roll=roll, pitch=pitch, wait=True)
-    input("Press enter to pick up")
-    con.pick_up()
-    input()
+def open_and_close_demo(con: Controller) -> None:
+    for _ in range(10):
+        con.add_to_queue(gripper_movement=GripperMovement.OPEN)
+        con.add_to_queue(gripper_movement=GripperMovement.CLOSE)
 
 
 def launch_gui(con: Controller) -> None:
@@ -76,33 +70,22 @@ def launch_gui(con: Controller) -> None:
         con.clear_queue()
 
     def pause() -> None:
-        # gray out itself and enable resume button
-        pause_button.config(state=tk.DISABLED)
-        resume_button.config(state=tk.NORMAL)
         con.pause()
 
     def resume() -> None:
-        # gray out itself and enable pause button
-        pause_button.config(state=tk.NORMAL)
-        resume_button.config(state=tk.DISABLED)
         con.resume()
 
     def close() -> None:
         root.destroy()
 
-    def safety() -> None:
-        threading.Thread(target=safety_demo, args=(con,)).start()
-
     def back_and_forth() -> None:
-        for _ in range(10):
-            con.go_sleep(wait=False)
-            con.go_home(wait=False)
+        threading.Thread(target=back_and_forth_demo, args=(con,)).start()
 
     def pick_and_place() -> None:
         threading.Thread(target=pick_and_place_demo, args=(con,)).start()
 
-    def pick_and_place2() -> None:
-        threading.Thread(target=pick_and_place_demo2, args=(con,)).start()
+    def open_and_close() -> None:
+        threading.Thread(target=open_and_close_demo, args=(con,)).start()
 
     menu = tk.Menu(root)
     root.config(menu=menu)
@@ -110,10 +93,33 @@ def launch_gui(con: Controller) -> None:
 
     demo_menu = tk.Menu(menu)
     menu.add_cascade(label="Demo", menu=demo_menu)
-    demo_menu.add_command(label="Safety", command=safety)
     demo_menu.add_command(label="Back and Forth", command=back_and_forth)
     demo_menu.add_command(label="Pick and Place", command=pick_and_place)
-    demo_menu.add_command(label="Pick and Place 2", command=pick_and_place2)
+    demo_menu.add_command(label="Open and Close", command=open_and_close)
+
+    # set up labels for current pose, current joint positions, gripper state, queue_size and update them every 100ms
+    pose_label = tk.Label(root, text="Pose: ")
+    pose_label.pack()
+    joint_label = tk.Label(root, text="Joint Positions: ")
+    joint_label.pack()
+    gripper_label = tk.Label(root, text="Gripper: ")
+    gripper_label.pack()
+    queue_size_label = tk.Label(root, text="Queue Size: ")
+    queue_size_label.pack()
+
+    def update_label() -> None:
+        pose_label.config(text=f"Pose: ({', '.join(str(round(x, 5)) for x in con.translation_rotation_pose)})")
+        joint_label.config(text=f"Joint Positions: ({', '.join(str(round(x, 5)) for x in con.joint_positions)})")
+        gripper_label.config(text=f"Gripper: {'Moving' if con.gripper_moving else 'Stopped'}")
+        queue_size_label.config(text=f"Queue Size: {con.queue_size}")
+        # update the button states if paused because the queue size might have changed
+        if con.paused:
+            pause_button.config(state=tk.DISABLED)
+            resume_button.config(state=tk.NORMAL)
+        else:
+            pause_button.config(state=tk.NORMAL)
+            resume_button.config(state=tk.DISABLED)
+        root.after(100, update_label)
 
     # add buttons for stop, pause, resume and quit
     stop_button = tk.Button(root, text="Clear Queue", command=stop)
@@ -125,6 +131,7 @@ def launch_gui(con: Controller) -> None:
     close_button = tk.Button(root, text="Close", command=close)
     close_button.pack()
     resume()
+    update_label()
 
     root.mainloop()
 
@@ -150,7 +157,6 @@ def main() -> None:
     # open_daemon(use_sim=True, ros2=True)
     controller = Controller(True)
     try:
-        # controller.set_xyz_pose(x=.2, y=0, z=.2, roll=0, pitch=0, wait=True)
         launch_gui(controller)
     except Exception as e:
         print(e)
