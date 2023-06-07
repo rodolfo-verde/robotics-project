883076932dd33c6d861e961edc8ef183af2bc6ae
Jonas <Jonas.schewior@gmail.com>
2023-06-07 17:28:47 +0200

    cnn change

--- audio_processing/speech_CNN.py
+++ audio_processing/speech_CNN.py
@@ -7,15 +7,36 @@ from keras.models import Sequential
 from keras import layers
 from keras.layers import Dense, Conv2D, Flatten, BatchNormalization, Dropout, MaxPooling2D
 #test 
-print(f"Tesorflow version {tf.__version__}")
+#print(f"Tesorflow version {tf.__version__}")
 
 # load data and split into trainings and test data
-data = # load data
-split = int(len(data)*0.8) # 80% trainings data, 20% test data
-X_train = data# load trainings data of voice samples
-y_train = # load labels of trainings data --> commands
+data = np.load(f"audio_processing\Train_Data\set1.npy",allow_pickle=True) # load data
+data_mfcc = data[:,0,0] # load mfccs, shape (amount of data --> int)
+data_labels = data[:,1,0] # load labels, shape (amount of labels --> int)
+split_mfcc = int(len(data_mfcc[:])*0.8) # 80% trainings data, 20% test data
+split_labels = int(len(data_labels[:])*0.8) # 80% trainings labels, 20% test labels
+X_train = data_mfcc[:split_mfcc] # load mfccs of trainings data, 80% of data
+X_test = data_mfcc[split_mfcc:]# load test mfcc data, 20% of data
+y_train = data_labels[:split_labels] # load train labels, 80% of labels
+y_test = data_labels[split_labels:] # load test labels, 20% of labels
 
-# CTC loss function	not implemented in Keras
+print(f"X_train shape: {X_train.shape}")
+print(f"X_test shape: {X_test.shape}")
+print(f"y_train shape: {y_train.shape}")
+print(f"y_test shape: {y_test.shape}")
+
+# reshape data
+X_train = X_train[0].reshape(12,70,len(X_train[0]))
+X_test = X_test[0].reshape(12,70,len(X_test[0]))
+y_train = y_train[0].reshape(9,1,len(y_train[0]))
+y_test = y_test[0].reshape(9,1,len(y_test[0]))
+
+print(f"X_train shape: {X_train.shape}")
+print(f"X_test shape: {X_test.shape}")
+print(f"y_train shape: {y_train.shape}")
+print(f"y_test shape: {y_test.shape}")
+
+"""# CTC loss function	not implemented in Keras
 def CTCLoss(y_true, y_pred):
     # Compute the training-time loss value
     batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
@@ -32,7 +53,7 @@ def CTCLoss(y_true, y_pred):
 model = Sequential()
 
 
-model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(28, 28, 1))) # input shape is ???
+model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(12,70))) # input shape is ???
 model.add(Flatten())
 model.add(BatchNormalization())
 model.add(Dense(10, activation="softmax"))
@@ -40,8 +61,8 @@ model.add(Dense(10, activation="softmax"))
 model.compile(optimizer="rmsprop", loss=CTCLoss, metrics=["accuracy"])
 
 model.fit(
-    X_train.reshape(-1, 28, 28, 1), 
+    X_train.reshape(12,70), 
     y_train, 
-    validation_data = (X_test.reshape(-1, 28, 28, 1), y_test),
+    validation_data = (X_test.reshape(12,70), y_test),
     epochs=30, 
-    batch_size=1000)
+    batch_size=1000)"""
