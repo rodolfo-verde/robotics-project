472c1a7df9735e1972f053a0d0b6f17faadec753
Jonas <Jonas.schewior@gmail.com>
2023-06-22 10:42:57 +0200

    minor changes CNN

--- audio_processing/speech_CNN.py
+++ audio_processing/speech_CNN.py
@@ -31,61 +31,23 @@ print(f"X_test shape: {X_test.shape}")
 print(f"y_train shape: {y_train.shape}")
 print(f"y_test shape: {y_test.shape}")
 
-# CTC loss function	not implemented in Keras
-def CTCLoss(y_true, y_pred):
-    # Compute the training-time loss value
-    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
-
-    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-
-    ctc_loss = keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
-    return ctc_loss
-
-#tf.nn.ctc_loss(
-#    labels,
-#    logits,
-#    label_length,
-#    logit_length,
-#    logits_time_major=True,
-#   unique=None,
-#    blank_index=None,
-#    name=None
-#)
-#ctc_loss = tf.nn.ctc_loss(y_train, X_train, label_length=y_train.shape, logit_length=X_train.shape, logits_time_major=True, unique=None, blank_index=None, name="ctc_loss_dense")
-
-# Regularization
-"""from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.regularizers import L2 
-model = Sequential([
-      Dropout(rate=0.1, input_shape=(784,)),
-      Dense(512, activation='relu', kernel_regularizer=L2(0.1)),
-      Dense(512, activation='relu', kernel_regularizer=L2(0.1)),
-      Dropout(rate=0.1, input_shape=(784,)),
-      Dense(10, activation="softmax")
-      ])"""
-
-#model.compile(optimizer=SGD(learning_rate=0.001), loss="categorical_crossentropy", metrics=['accuracy'])
-
-
 
 
 # CNN
 model = Sequential()
 
-
 model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(11,70,1))) 
-model.add(Flatten())
-model.add(BatchNormalization())
-model.add(Dropout(0.02))
-model.add(Dense(10, activation="sigmoid"))
+model.add(Dropout(0.1))
 model.add(BatchNormalization())
 model.add(Flatten())
+model.add(Dense(10, activation="sigmoid", kernel_regularizer=L2(0.1)))
+model.add(Dropout(0.1))
+#model.add(BatchNormalization())
+#model.add(Flatten())
+#model.add(Dropout(0.1))
 model.add(Dense(9, activation="softmax"))
 
-model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam         loss = categorical_crossentropy, CTCLoss
+model.compile(optimizer=SGD(learning_rate = 0.001), loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam     loss = categorical_crossentropy, CTCLoss
 
 result = model.fit(
     X_train.reshape(-1, 11, 70, 1), 
