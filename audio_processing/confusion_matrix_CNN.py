1963f35ed357a987c33f6c03d26ed8692f1b2054
Roman Grefen <rgrefen96@gmail.com>
2023-07-05 16:14:57 +0200

    small changes

--- audio_processing/confusion_matrix_CNN.py
+++ audio_processing/confusion_matrix_CNN.py
@@ -13,80 +13,23 @@ from keras.layers import Dense, Conv2D, Flatten, BatchNormalization, Dropout, Ma
 from keras.optimizers import SGD
 from keras.regularizers import L2 
 
-test_again = False
+# CNN
+model = Sequential()
 
-if test_again:
-    data_set_name = "data_set_roman_complete"
-    # load data and split into trainings and test data
-    data_mfcc = np.load(f"audio_processing/Train_Data/{data_set_name}_mfcc.npy",allow_pickle=True) # load data
-    data_labels = np.load(f"audio_processing/Train_Data/{data_set_name}_label.npy",allow_pickle=True) # load data
+model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(11,70,1), padding="same"))
+model.add(MaxPooling2D(pool_size=(5, 5), padding="same"))
+model.add(BatchNormalization())
+model.add(Dropout(0.1))
+model.add(Flatten())
+model.add(Dense(10, activation="sigmoid", kernel_regularizer=L2(0.01), bias_regularizer=L2(0.01)))
+model.add(Dense(9, activation="softmax"))
 
-    print(f"Data shape: {data_mfcc.shape}")
-    print(f"Labels shape: {data_labels.shape}")
+model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam     loss = categorical_crossentropy, CTCLoss
 
-    split_mfcc = int(len(data_mfcc[:,10,69])*0.8) # 80% trainings data, 20% test data
-    split_labels = int(len(data_labels[:,8])*0.8) # 80% trainings labels, 20% test labels
-    X_train = data_mfcc[:split_mfcc] # load mfccs of trainings data, 80% of data
-    X_test = data_mfcc[split_mfcc:]# load test mfcc data, 20% of data
-    y_train = data_labels[:split_labels] # load train labels, 80% of labels
-    y_test = data_labels[split_labels:] # load test labels, 20% of labels
+#import weights
+model.load_weights("audio_processing/speech_CNN_weights.h5")
 
-    print(f"X_train shape: {X_train.shape}")
-    print(f"X_test shape: {X_test.shape}")
-    print(f"y_train shape: {y_train.shape}")
-    print(f"y_test shape: {y_test.shape}")
-
-if test_again:
-    model = Sequential()
-    model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(11,70,1))) 
-    model.add(Flatten())
-    model.add(BatchNormalization())
-    model.add(Dropout(0.02))
-    model.add(Dense(10, activation="sigmoid"))
-    model.add(BatchNormalization())
-    model.add(Flatten())
-    model.add(Dense(9, activation="softmax"))
-
-    model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam         loss = categorical_crossentropy, CTCLoss
-
-
-if not test_again:
-    # CNN
-    model = Sequential()
-
-    model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(11,70,1))) 
-    model.add(Dropout(0.1))
-    model.add(BatchNormalization())
-    model.add(Flatten())
-    model.add(Dense(10, activation="sigmoid", kernel_regularizer=L2(0.1)))
-    model.add(Dropout(0.1))
-    #model.add(BatchNormalization())
-    #model.add(Flatten())
-    #model.add(Dropout(0.1))
-    model.add(Dense(9, activation="softmax"))
-
-    model.compile(optimizer=SGD(learning_rate = 0.001), loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam     loss = categorical_crossentropy, CTCLoss
-
-
-if not test_again:
-
-    #import weights
-    model.load_weights("audio_processing/speech_CNN_weights.h5")
-
-if test_again:
-    result = model.fit(
-        X_train.reshape(-1, 11, 70, 1), 
-        y_train, 
-        validation_data = (X_test.reshape(-1, 11, 70, 1), y_test),
-        epochs=30, 
-        batch_size=10)
-
-    # evaluate model
-    test_loss, test_acc = model.evaluate(X_test.reshape(-1, 11, 70, 1), y_test, verbose=2)
-    print(f"Test accuracy: {test_acc}")
-
-
-data_test_set_name = "set_complete_test"
+data_test_set_name = "data_set_roman_complete"
 # predict
 class_names = ["a", "b", "c", "1", "2", "3", "rex", "stopp", "other"]
 predict_mfcc = np.load(f"audio_processing/Train_Data/{data_test_set_name}_mfcc.npy",allow_pickle=True) # load data
