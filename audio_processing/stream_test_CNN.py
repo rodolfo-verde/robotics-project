e25e753c4d99c73a96d9e858204f59b75cfd1006
Jonas <Jonas.schewior@gmail.com>
2023-07-05 15:39:01 +0200

    new sheisse

--- audio_processing/stream_test_CNN.py
+++ audio_processing/stream_test_CNN.py
@@ -21,74 +21,22 @@ from os import listdir
 from dataprocessor import dataprocessor
 from mfcc_processor import mfcc_dataprocessor
 
-test_again = False
+# CNN
+model = Sequential()
 
-if test_again:
-    # load data and split into trainings and test data
-    data_mfcc = np.load(f"audio_processing/Train_Data/train_test_mfcc.npy",allow_pickle=True) # load data
-    data_labels = np.load(f"audio_processing/Train_Data/train_test_label.npy",allow_pickle=True) # load data
+model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(11,70,1), padding="same"))
+model.add(MaxPooling2D(pool_size=(5, 5), padding="same"))
+model.add(BatchNormalization())
+model.add(Dropout(0.1))
+model.add(Flatten())
+model.add(Dense(10, activation="sigmoid", kernel_regularizer=L2(0.01), bias_regularizer=L2(0.01)))
+model.add(Dense(9, activation="softmax"))
 
-    print(f"Data shape: {data_mfcc.shape}")
-    print(f"Labels shape: {data_labels.shape}")
-
-    split_mfcc = int(len(data_mfcc[:,10,69])*0.8) # 80% trainings data, 20% test data
-    split_labels = int(len(data_labels[:,8])*0.8) # 80% trainings labels, 20% test labels
-    X_train = data_mfcc[:split_mfcc] # load mfccs of trainings data, 80% of data
-    X_test = data_mfcc[split_mfcc:]# load test mfcc data, 20% of data
-    y_train = data_labels[:split_labels] # load train labels, 80% of labels
-    y_test = data_labels[split_labels:] # load test labels, 20% of labels
-
-    print(f"X_train shape: {X_train.shape}")
-    print(f"X_test shape: {X_test.shape}")
-    print(f"y_train shape: {y_train.shape}")
-    print(f"y_test shape: {y_test.shape}")
-
-if test_again:
-    model = Sequential()
-    model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(11,70,1))) 
-    model.add(Flatten())
-    model.add(BatchNormalization())
-    model.add(Dropout(0.02))
-    model.add(Dense(10, activation="sigmoid"))
-    model.add(BatchNormalization())
-    model.add(Flatten())
-    model.add(Dense(9, activation="softmax"))
-
-    model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam         loss = categorical_crossentropy, CTCLoss
+model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam     loss = categorical_crossentropy, CTCLoss
 
 
-if not test_again:
-    # CNN
-    model = Sequential()
-
-    model.add(Conv2D(10, kernel_size=(3, 3), activation="sigmoid", input_shape=(11,70,1), padding="same"))
-    model.add(MaxPooling2D(pool_size=(5, 5), padding="same"))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.1))
-    model.add(Flatten())
-    model.add(Dense(10, activation="sigmoid", kernel_regularizer=L2(0.01), bias_regularizer=L2(0.01)))
-    model.add(Dense(9, activation="softmax"))
-
-    model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]) # optimizer = rmsprop, Adam     loss = categorical_crossentropy, CTCLoss
-
-
-
-if not test_again:
-
-    #import weights
-    model.load_weights("audio_processing/speech_CNN_weights.h5")
-
-if test_again:
-    result = model.fit(
-        X_train.reshape(-1, 11, 70, 1), 
-        y_train, 
-        validation_data = (X_test.reshape(-1, 11, 70, 1), y_test),
-        epochs=30, 
-        batch_size=50)
-
-    # evaluate model
-    test_loss, test_acc = model.evaluate(X_test.reshape(-1, 11, 70, 1), y_test, verbose=2)
-    print(f"Test accuracy: {test_acc}")
+#import weights
+model.load_weights("audio_processing\speech_CNN_weights.h5")
 
 BLOCKLENGTH = 44100
 SAMPLERATE = 44100
@@ -138,7 +86,17 @@ with stream:
             mfcc = mp.mfcc_process(i)
             prediction = model.predict(mfcc[1:].reshape(-1, 11, 70, 1))
             index_pred = np.argmax(prediction) #tf.argmax geht auch
-            print(f"Prediction: {class_names[index_pred]} and {prediction}")
+            print(f"Prediction: {class_names[index_pred]} and {prediction[0][index_pred]*100} %")
+            print(f"Word: {class_names[0]} equals a prediction of {prediction[0][0]*100} %")
+            print(f"Word: {class_names[1]} equals a prediction of {prediction[0][1]*100} %")
+            print(f"Word: {class_names[2]} equals a prediction of {prediction[0][2]*100} %")
+            print(f"Word: {class_names[3]} equals a prediction of {prediction[0][3]*100} %")
+            print(f"Word: {class_names[4]} equals a prediction of {prediction[0][4]*100} %")
+            print(f"Word: {class_names[5]} equals a prediction of {prediction[0][5]*100} %")
+            print(f"Word: {class_names[6]} equals a prediction of {prediction[0][6]*100} %")
+            print(f"Word: {class_names[7]} equals a prediction of {prediction[0][7]*100} %")
+            print(f"Word: {class_names[8]} equals a prediction of {prediction[0][8]*100} %")
+            print(f"Time: {time.time()-starttime}")
         
         print(time.time()-starttime)
 
