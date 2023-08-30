eca5ebddb74919086ed0a217b0d4ae532a264cdd
Jonas <Jonas.schewior@gmail.com>
2023-08-30 15:02:25 +0200

    i got a plan z if all things go to shit speech_recognition works

--- audio_processing/speechtest.py
+++ audio_processing/speechtest.py
@@ -1,6 +1,5 @@
 import speech_recognition as sr  
 import pyttsx3
-import time
 
 # Initialize the recognizer
 r = sr.Recognizer()
@@ -25,35 +24,8 @@ with sr.Microphone() as source2:
     audio2 = r.listen(source2)
 
 # Using google to recognize audio
-MyText = r.recognize_google(audio2)
+MyText = r.recognize_google(audio2, language='de-DE')
 MyText = MyText.lower()
 
 print("Did you say "+MyText)
 SpeakText(MyText)
-
-
-"""for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print('Microphone with name "{1}" found for `Microphone(device_index={0})`'.format(index, name))
-"""
-"""# get audio from the microphone                                                                       
-r = sr.Recognizer()                                                                                   
-with sr.Microphone(device_index=1) as source:                                                                       
-    print("Speak:")
-    starttime = time.time()                                                                                   
-    audio = r.listen(source)   
-    endtime = time.time()
-    print(f"Time needed for listening: {endtime-starttime}")
-    print("Starting to recognize...")
-    starttime = time.time()
-    print("You said " + r.recognize_google(audio))
-    print(f"Time needed: {time.time()-starttime}")"""
-
-"""try:
-    print("Starting to recognize...")
-    starttime = time.time()
-    print("You said " + r.recognize_google(audio))
-    print(f"Time needed: {time.time()-starttime}")
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))"""
