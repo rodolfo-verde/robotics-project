4d55f472f3a1cd69a8ce27f501910c436105098a
Jonas <Jonas.schewior@gmail.com>
2023-09-01 10:57:41 +0200

    spectrograms also work i just need more data lesssgoooo!!

--- audio_processing/data_spectrogramm.py
+++ audio_processing/data_spectrogramm.py
@@ -3,18 +3,7 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 import tensorflow as tf
 
-# load wave file for processing
-wave = np.load("audio_processing\Train_Data\set5_raw.npy",allow_pickle=True) # load data
-# choose the first 200 words of the wave file
-wave = wave[:200]
-#labels = np.load("audio_processing\Train_Data\set_eins_200_label.npy",allow_pickle=True) # load data
-print(f"Data shape: {wave.shape}")
-#print(f"Labels shape: {labels.shape}")
 
-# plot first word of wave
-plt.figure()
-plt.plot(wave[0])
-plt.show()
 
 def get_spectrogram(waveform):
   # Convert the waveform to a spectrogram via a STFT.
@@ -24,9 +13,7 @@ def get_spectrogram(waveform):
   spectrogram = tf.abs(spectrogram)
   return spectrogram
 
-# convert wave to spectrogram
-spectrogram = get_spectrogram(wave[0])
-print(f"Spectrogram shape: {spectrogram.shape}")
+
 
 def plot_spectrogram(spectrogram, ax):
   if len(spectrogram.shape) > 2:
@@ -42,25 +29,5 @@ def plot_spectrogram(spectrogram, ax):
   Y = range(height)
   ax.pcolormesh(X, Y, log_spec)
 
-# plot spectrogram
-fig, axes = plt.subplots(2, figsize=(12, 8))
-timescale = np.arange(wave[0].shape[0])
-axes[0].plot(timescale, wave[0])
-axes[0].set_title('Waveform')
-axes[0].set_xlim([0, 16000])
 
-plot_spectrogram(spectrogram.numpy(), axes[1])
-axes[1].set_title('Spectrogram')
-#plt.suptitle("Command: Eins")
-plt.show()
-
-# convert the whole wave file into spectrograms
-spectrograms = []
-for i in range(wave.shape[0]):
-    spectrograms.append(get_spectrogram(wave[i]))
-spectrograms = np.array(spectrograms)
-print(f"Spectrograms shape: {spectrograms.shape}")
-
-# save spectrograms as .npy file
-np.save(f"audio_processing\Train_Data\set_other_200_spectrogram.npy", spectrograms) # X, 252,129 = shape of spectrograms
 
