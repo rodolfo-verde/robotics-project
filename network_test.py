44a63631a656de4471412df5fcd1d36feebab5a1
Jonas-Schewior <117079033+Jonas-Schewior@users.noreply.github.com>
2024-05-15 09:43:31 +0200

    Update network_test.py

--- network_test.py
+++ network_test.py
@@ -1,3 +1,7 @@
+print("test")
+
+import time
+
 import os
 import numpy as np
 import tensorflow as tf 
@@ -9,7 +13,6 @@ from keras.layers import Dense, Conv2D, Flatten, BatchNormalization, Dropout, Ma
 from keras.optimizers import SGD
 from keras.regularizers import L2 
 from keras.models import load_model
-import time
 import sounddevice as sd
 
 from audio_processing.dataprocessor import dataprocessor
@@ -26,25 +29,6 @@ import I2C_LCD_driver
 import queue
 import threading
 
-# import big speech model
-#model.load_weights("audio_processing//CNN_Models//CNN_Model_Rex_simple.h5")
-# Load the saved model
-print("Loading model...")
-
-try:
-    model = tf.keras.models.load_model('audio_processing//CNN_Models//CNN_Model_Rex_simple.h5')
-    model.summary()
-except Exception as e:
-    print(e)
-    quit()
-
-print("Model loaded.")
-
-#safe1 stores the input from the stream to be processed later
-        
-safe1 = np.array([], dtype="float64")
-
-
 # This is the Network class
 # It will be used to run the game
 
@@ -57,43 +41,104 @@ class Network:
         self.mylcd = I2C_LCD_driver.lcd()
         self.mylcd.lcd_clear()
         self.wordlogic = WordLogic(self.mylcd)
-        self.ticktacktoe = TickTackToe(solo_play=False, display = None)
+        self.ticktacktoe = None
+        self.round = 0
+        self.game_type = False
 
-    def run(self):
-        try:                
+    def run(self):            
+        try:
+            while self.ticktacktoe == None:
+                try:                    
+                    self.mylcd.lcd_display_string(f"1 = Alleine!",1)
+                    self.mylcd.lcd_display_string(f"2 = Zu zweit!",2)
+                    command = self.audio_stream.command_queue.get_nowait()
+                    if command == "1":
+                        command = None
+                        #print("I got here")
+                        self.ticktacktoe = TickTackToe(solo_play=True, start = 0, display = None)
+                        #print("I got even further")
+                        self.mylcd.lcd_clear()
+                        self.game_type = True
+                    elif command == "2":
+                        command = None
+                        self.ticktacktoe = TickTackToe(solo_play=False, display = None)
+                        self.mylcd.lcd_clear()
+                        self.game_type = True
+                    else:
+                        continue
+                except queue.Empty:
+                    time.sleep(0.1)                
             # this runs the game
             def check_for_stop_command():
                 while True:
                     try:
                         # Try to get a command from the stop_command_queue without blocking
                         command = self.audio_stream.stopp_command_queue.get_nowait()
-                        if command == "Stopp":
+                        if command == "stopp":
                             print("Stopping the robot...")
-                            self.ticktacktoe.command("Stopp")
+                            self.ticktacktoe.command("Stopp")                            
                     except queue.Empty:
                         time.sleep(0.1)
             
             # Start the thread that checks for the "Stopp" command
             stop_thread = threading.Thread(target=check_for_stop_command, daemon=True)
             stop_thread.start()
-            while True:
+            while self.game_type == True:
+            #while True:
                 try:
-                    command = self.audio_queue.get_nowait()
-                    print(f"Command from audio_queue: {command}")
-                    self.wordlogic.command(command)
-                    if self.wordlogic.get_combination() not in ["", None]:
-                        self.ticktacktoe.command(self.wordlogic.get_combination())
-                        while self.ticktacktoe.playing:
-                            self.mylcd.lcd_display_string(f"REX     {self.wordlogic.get_combination().upper()}     %",2)
-                            time.sleep(.21)
-                            self.mylcd.lcd_display_string(f"REX     {self.wordlogic.get_combination().upper()}      ",2)
-                            time.sleep(.21)                
-                        self.wordlogic.reset_combination()
-                    elif command == "Other":
-                        pass
+                    if not self.ticktacktoe._game_over:
+                        self.switch_player()
+                        
+                        command = self.audio_stream.command_queue.get_nowait()
+                        #print(f"Command from audio_queue: {command}")
+                        self.wordlogic.command(command)                        
+                            if self.wordlogic.get_combination() not in ["", None] && not self.ticktacktoe.rex_playing:
+                                self.ticktacktoe.command(self.wordlogic.get_combination())
+                                while self.ticktacktoe.playing:
+                                    self.mylcd.lcd_display_string(f"REX     {self.wordlogic.get_combination().upper()}     %",2)
+                                    time.sleep(.21)
+                                    self.mylcd.lcd_display_string(f"REX     {self.wordlogic.get_combination().upper()}      ",2)
+                                    time.sleep(.21)
+                                self.wordlogic.reset_combination()
+                                if self.ticktacktoe.cmd == "Stopp":
+                                    self.mylcd.lcd_display_string(f"REX     Stoppt!     %",2)
+                                    time.sleep(2)                
+                                self.mylcd.lcd_clear()
+                            elif command == "Other":
+                                pass
+                    else:
+                        print(self.ticktacktoe._check_draw())
+                        if self.ticktacktoe._check_draw():
+                            self.mylcd.lcd_clear()
+                            self.mylcd.lcd_display_string(f"Unentschieden!",1)
+                            self.mylcd.lcd_display_string(f"Rex raeumt auf!",2)
+                            self.game_type = False
+                            #self.ticktacktoe = None
+                        elif np.abs(self.ticktacktoe._current_player-1) == 0:
+                            self.mylcd.lcd_clear()
+                            self.mylcd.lcd_display_string(f"Rot gewinnt!",1)
+                            self.mylcd.lcd_display_string(f"Rex raeumt auf!",2)
+                            self.game_type = False
+                            #self.ticktacktoe = None
+                        else:
+                            self.mylcd.lcd_clear()
+                            self.mylcd.lcd_display_string(f"Blau gewinnt!",1)
+                            self.mylcd.lcd_display_string(f"Rex raeumt auf!",2)
+                            self.game_type = False
+                            #self.ticktacktoe = None
+                                                                                  
                 except queue.Empty:
                     time.sleep(0.1)
                     continue
+            #print("hekko")
+            while self.ticktacktoe._resetplay == 1:
+                time.sleep(0.1)
+                #print("cleaning")                
+            else:
+                #print("dead")
+                self.ticktacktoe = None
+                self.mylcd.lcd_clear()
+                self.run()
         except KeyboardInterrupt:
             print("Interrupted by user...")
         except Exception as e:
@@ -103,11 +148,22 @@ class Network:
     def start_audio_stream(self):
         self.audio_stream_thread = threading.Thread(target=self.audio_stream.main, daemon=True)
         self.audio_stream_thread.start()
+    
+    def switch_player(self):
+        if self.ticktacktoe.rex_playing:
+            self.mylcd.lcd_display_string(f"Rex spielt!",1)
+        elif self.ticktacktoe._current_player == 0:
+            self.mylcd.lcd_display_string(f"Rot Spielt!",1)
+        else:
+            self.mylcd.lcd_display_string(f"Blau Spielt!",1)
+            
+        
 
                     
 if __name__ == '__main__':
     network = Network()
     network.start_audio_stream()
     network.run()
+    
     
     
